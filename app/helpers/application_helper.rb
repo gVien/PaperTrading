d17c3e1a6b639b6679db82f9e117a10f1724e9da
gVien <wavetech86@gmail.com>
2015-11-01 13:37:45 -0800

    add full_title method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # this helper gives the full page title on a per page basis
+  def full_title(page_title="")
+    base_title = "Learn to Trade with PaperTrading"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
