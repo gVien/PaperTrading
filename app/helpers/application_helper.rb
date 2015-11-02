af224a526472977ed2a3247e2c16cfa523221a88
gVien <wavetech86@gmail.com>
2015-11-02 00:45:40 -0800

    add format number helper for market cap method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  # modified number_to_human helper method
+  # http://api.rubyonrails.org/classes/ActionView/Helpers/NumberHelper.html
+  def format_number(market_cap)
+    number_to_human(market_cap, precision: 4, units: {million: "M", billion: "B"}).split(" ").join("") # get rid of spacing in between
+  end
 end
