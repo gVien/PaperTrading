efc880e6677a60ab434efacc42657a71b75bb0c8
gVien <wavetech86@gmail.com>
2015-11-02 23:55:40 -0800

    add new york times model & methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @market_summary = market_summary
     @market_trend_pct = market_trend
+    @ny_times = NewYorkTime.get_business_news
   end
 
   def about
