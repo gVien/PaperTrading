dd8fd508f4c7fe034d6e38cd3510aa1df669cae7
gVien <wavetech86@gmail.com>
2015-11-03 00:49:28 -0800

    add ny times api market news to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @market_summary = market_summary
     @market_trend_pct = market_trend
-    @ny_times = NewYorkTime.get_business_news
+    @ny_times = NewYorkTime.get_business_news.first(10)
   end
 
   def about
