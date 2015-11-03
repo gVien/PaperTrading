385627afa0dfec4547b39db109a9a2bcd1200813
gVien <wavetech86@gmail.com>
2015-11-03 13:22:10 -0800

    refactor codes in home page & rename market trend variable

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @market_summary = market_summary
-    @market_trend_pct = market_trend
+    @market_trend = market_trend
     @ny_times = NewYorkTime.get_business_news.first(10)
   end
 
