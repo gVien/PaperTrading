c48747aa1bd11fe780525f6e05ab54634933f327
gVien <wavetech86@gmail.com>
2015-11-04 14:35:05 -0800

    add stock test criteria

--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class StockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @watchlist = watchlists(:watchlist1)
+    @stock = @watchlist.stocks.build(symbol: "TSLA", company_name: "Tesla Motor")
+  end
+
+  # test criteria
+  # symbol must be valid and present
+  # symbol must be upper case
+  # company_name must be valid and present
+  # stock must belong to watchlist id
+  # watchlist id must be present
 end
