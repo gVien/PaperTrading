d33db4b8030876f7732f566466397f3a6e29e3e1
gVien <wavetech86@gmail.com>
2015-11-04 13:28:44 -0800

    add digest method to generate a hash digest, similar to bcrypt password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, :length => { maximum: 255 }, :format => { with: VALID_EMAIL_REGEX }, :uniqueness => { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.digest(string)
+    # https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
+    # source from the above link
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    # string is the string that needs to be hash and cost is the computational cost to calculate the hash. The higher the cost, it will be harder to determine the original password
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
