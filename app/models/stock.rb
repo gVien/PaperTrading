9bcbfbf674624d3c33dbe3f935a5a70571dd3eee
gVien <wavetech86@gmail.com>
2015-11-04 15:19:48 -0800

    add validation to pass test for stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,13 @@
 class Stock < ActiveRecord::Base
   belongs_to :watchlist
+  before_save :upcase_symbol
 
+  validates :symbol, presence: true
+  validates :company_name, presence: true
+  validates :watchlist_id, presence: true
+
+  private
+    def upcase_symbol
+      self.symbol = symbol.upcase
+    end
 end
