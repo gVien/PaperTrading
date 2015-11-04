2f46cef6e87884d4ea12149bd7be2bff6325ced2
gVien <wavetech86@gmail.com>
2015-11-04 15:19:36 -0800

    add stock TDD test

--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -7,9 +7,34 @@ class StockTest < ActiveSupport::TestCase
   end
 
   # test criteria
+  # creating row should be valid
+  test "should be valid" do
+    assert @stock.valid?
+  end
+
   # symbol must be valid and present
+  test "symbol should be valid (present)" do
+    @stock.symbol = "    "
+    assert_not @stock.valid?   #pass if
+  end
+
+  # company name should valid
+  test "company name should be valid" do
+    @stock.company_name = "   "
+    assert_not @stock.valid?
+  end
+
   # symbol must be upper case
-  # company_name must be valid and present
+  test "symbol should be uppercase before saving to database" do
+    new_stock = @watchlist.stocks.build(symbol: "goog", company_name: "Alphabet Inc.")
+    new_stock.save
+    assert_equal  "GOOG", new_stock.reload.symbol
+  end
+
   # stock must belong to watchlist id
   # watchlist id must be present
+  test "watchlist id must be present" do
+    @stock.watchlist_id = nil
+    assert_not @stock.valid?
+  end
 end
