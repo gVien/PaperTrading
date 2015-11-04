e4b93b8f71e69caf52c6910fc2ef020d54e4376c
gVien <wavetech86@gmail.com>
2015-11-03 19:51:07 -0800

    add test for new york time model & minor fix to model

--- app/models/new_york_time.rb
+++ app/models/new_york_time.rb
@@ -1,4 +1,4 @@
-class NewYorkTime < ActiveRecord::Base
+class NewYorkTime
 
   def self.get_business_news
     response = HTTParty.get("http://api.nytimes.com/svc/topstories/v1/business.json?api-key=#{ENV['NY_KEY']}")
--- test/models/new_york_time_test.rb
+++ test/models/new_york_time_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class NewYorkTimeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not return a nil data" do
+    news = NewYorkTime.get_business_news
+    assert_not(news.nil?) # must not be nil
+  end
 end
