d19e105a1f17700f267728aa7851ab0738d3bfc3
gVien <wavetech86@gmail.com>
2015-11-04 14:00:03 -0800

    add note to clarify indexing 2 columns

--- db/migrate/20151104204338_create_microposts.rb
+++ db/migrate/20151104204338_create_microposts.rb
@@ -7,6 +7,10 @@ class CreateMicroposts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    # adds index on user_id and created_at columns since we expect to reverse the order of the microposts with a given user_id
+    # By including both the user_id and created_at columns as an array,
+    # we arrange for Rails to create a multiple key index,
+    # which means that Active Record uses both keys at the same time.
     add_index :microposts, [:user_id, :created_at]
   end
 end
