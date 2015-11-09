1664d7f44122d368d412a3e915d8e42733531856
gVien <wavetech86@gmail.com>
2015-11-08 23:58:59 -0800

    add current_user helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # method to return current user that is in session (if any)
+  def current_user
+    # find method raises an error if session[:user_id] is nil
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
