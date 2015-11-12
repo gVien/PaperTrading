e856d3b770cd63a8c7b5b78a65d0122bef2542d7
gVien <wavetech86@gmail.com>
2015-11-12 14:39:36 -0800

    update index integration test for delete links and ability to delete

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,22 +2,30 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @gai = users(:gai)
+    @gai_admin = users(:gai)
+    @non_admin_user = users(:second_user)
   end
 
   # tests the pagination page
-  # 1. login in as a user
+  # 1. login in as an admin user
   # 2. get the user index page
   # 3. verify the index template renders properly
   # 4. verify the div pagination is on the page
   # 5. verify the link of each pagination works and each text is equal to the user full name
-  test "index including pagination" do
-    log_in_as(@gai)
+  # 6. verify that the delete link exists
+  # 7. verify that the user count is 1 less (-1) after deleting a user
+  test "index including pagination and presents of delete text" do
+    log_in_as(@gai_admin)
     get users_path
     assert_template("users/index")
     assert_select "div.pagination"
     User.paginate(page: 1).each do |user|
       assert_select "a[href=?]", user_path(user), text: full_name(user)
+      # admin user cannot see the delete button next to his or her name
+      assert_select("a[href=?]", user_path(user), text: "delete") unless user.admin? # if user is not admin
+    end
+    assert_difference "User.count", -1 do
+      delete user_path(@non_admin_user)
     end
   end
 end
