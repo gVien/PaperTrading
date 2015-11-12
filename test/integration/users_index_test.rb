ea51f3fddadcd4aae264966fd6d4d0ddafc3236a
gVien <wavetech86@gmail.com>
2015-11-12 13:37:47 -0800

    name should be full name

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -10,14 +10,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   # 2. get the user index page
   # 3. verify the index template renders properly
   # 4. verify the div pagination is on the page
-  # 5. verify the link of each pagination works and each text is equal to the user name
+  # 5. verify the link of each pagination works and each text is equal to the user full name
   test "index including pagination" do
-    log_in_as(@user)
+    log_in_as(@gai)
     get users_path
     assert_template("users/index")
     assert_select "div.pagination"
     User.paginate(page: 1).each do |user|
-      assert_select "a[href=?]", user_path(user), text: user.name
+      assert_select "a[href=?]", user_path(user), text: full_name(user)
     end
   end
 end
