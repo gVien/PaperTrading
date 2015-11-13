37b62c6043a8272c01c528f9d5af98f98b1eb044
gVien <wavetech86@gmail.com>
2015-11-12 20:18:46 -0800

    forgot the input parameter in account activation method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 
   # sends activation email
-  def account_activation
+  def account_activation(user)
     @user = user  # instance variable so the mailer view can have access
     mail to: user.email, subject: "Activate your PaperTrading account"  # sending the email to this user
   end
