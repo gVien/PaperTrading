23763a7b8f4d483beca4f83b397b96a6ddb210aa
gVien <wavetech86@gmail.com>
2015-11-16 14:38:57 -0800

    fill out edit form, add find_user and validate user is valid in reset ctrler

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
+
   def new
   end
 
@@ -15,6 +19,30 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  # edit/update apply to these 4 cases
+  # 1. a reset link is expired (edit/update)
+  # 2. a reset is successful (update)
+  # 3. a failed reset (invalid password) - applies to update
+  # 4. a failed reset that seems successful initially (a blank password) - applies to update
   def edit
   end
+
+  def update
+  end
+
+  private
+    # find the user using email via hidden field
+    def find_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # confirm the user account is activated, exist, and authenticated w/ correct reset token
+    def valid_user
+       # /password_resets/:id/edit?email=encoded_email
+       # /password_resets/0enrhAa0CyycHEUxJvCAiA/edit?email=example%40example.com
+      unless @user.activated? && @user && @user.authenticated?(:reset_digest, params[:id])
+        flash[:danger] = "Password reset is invalid."
+        redirect_to root_url
+      end
+    end
 end
