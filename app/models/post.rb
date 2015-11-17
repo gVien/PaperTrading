cdc8388ae64d14858c207bff78085dd2a37c936d
gVien <wavetech86@gmail.com>
2015-11-17 14:49:07 -0800

    clarify the stabby lambda comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   # -> is “stabby lambda” syntax for an object called a Proc (procedure) or lambda
+  # equivalent to default_scope lambda { order(created_at: :desc) }
   default_scope -> { order(created_at: :desc) } # redefine the default order of posts
 
   validates :content, presence: true, length: { maximum: 140 }
