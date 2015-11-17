e61eb906c3ab96cb1eaeeea3a69994b4173fff30
gVien <wavetech86@gmail.com>
2015-11-17 14:41:00 -0800

    reverse all posts order DESC to pass post model test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  # -> is “stabby lambda” syntax for an object called a Proc (procedure) or lambda
+  default_scope -> { order(created_at: :desc) } # redefine the default order of posts
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
