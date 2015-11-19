41ac78b016558c8a2e0538b5d4436a773c906b00
gVien <wavetech86@gmail.com>
2015-11-18 22:31:34 -0800

    add posts route & post ctrler test to only allow loggedin user to create/destroy

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:newest_post)
+  end
+
+  test "should redirect create if user is not logged in" do
+    assert_no_difference "Post.count" do
+      post(:create, post: { content: "This cannot be created"})
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy if user is not logged in" do
+    assert_no_difference "Post.count" do
+      delete(:destroy, id: @post)
+    end
+    assert_redirected_to login_url
+  end
 end
