01db90c7771bfaa501dac6665f53415885c0f17a
gVien <wavetech86@gmail.com>
2015-11-20 00:23:41 -0800

    test destroy feature in post controller test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
   def setup
-    @post = posts(:newest_post)
+    @gai = users(:gai)
+    @post = posts(:newest_post)  # post belong to @gai
+    @second_user = users(:second_user)
+    @food = posts(:food)  # post belong to @second_user
   end
 
   test "should redirect create if user is not logged in" do
@@ -17,5 +20,17 @@ class PostsControllerTest < ActionController::TestCase
       delete(:destroy, id: @post)
     end
     assert_redirected_to login_url
+  end
+
+  # test flow
+  # 1. login as @gai
+  # 2. verify @second_user cannot delete @post made by @gai
+  # 3. verify the page is redirected to root
+  test "should redirect destroy if user is not the current user who made the post" do
+    log_in_as(@gai)
+    assert_no_difference("Post.count") do
+      delete(:destroy, id: @food)
+    end
+    assert_redirected_to root_url # root_url defined in check_correct_user_post method
   end
 end
