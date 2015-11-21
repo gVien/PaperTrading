8788e4306fe60e85e69e28ac31acf2084b245cf4
gVien <wavetech86@gmail.com>
2015-11-20 23:59:17 -0800

    add picture upload feature in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   # -> is “stabby lambda” syntax for an object called a Proc (procedure) or lambda
   # equivalent to default_scope lambda { order(created_at: :desc) }
   default_scope -> { order(created_at: :desc) } # redefine the default order of posts
+  mount_uploader :picture, PictureUploader # mount the migrated picture column to carrierwave PictureUploader
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
