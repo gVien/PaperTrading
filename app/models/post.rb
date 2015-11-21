2348aaa78723f5e7406ecb71b4e9b0a1dc23b664
gVien <wavetech86@gmail.com>
2015-11-21 00:27:02 -0800

    add client + server sides validation for image extension and size

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,13 @@ class Post < ActiveRecord::Base
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+  validate :cap_picture_size
+
+  private
+    # server side validation for picture size (need client side too)
+    def cap_picture_size
+      if self.picture.size > 5.megabytes  #self is optional
+        errors.add("Image size must be 5MB or less.")
+      end
+    end
 end
