ea4d1835aa872171e4f7906080cbc178d2dd6858
gVien <wavetech86@gmail.com>
2015-11-26 00:03:42 -0800

    disable post button if post form or file upload is empty, add style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,31 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+
+  var textField = $("#post_content"),
+      btnPost   = $("#btn-post"),
+      postPix   = $("#post_picture");
+
+  // client-side disable of post btn if
+  // 1. text field is empty WITHOUT picture upload
+  // 2. text field is blank (e.g. "  ") WITHOUT picture upload
+
+  textField.keyup(function() {
+    if(!$.trim($(this).val()) && !postPix.val()) {  // if not empty/blank && file not blank
+       btnPost.attr("disabled", true); // or .prop
+    } else if ($.trim($(this).val())) {
+       btnPost.attr("disabled", false);
+    }
+  });
+
+  // enable post btn if a picture upload exists
+  postPix.change(function() {
+    if ($(this).val()) {
+      btnPost.attr("disabled", false);
+    } else if ($(this).val() === "") {
+      btnPost.attr("disabled", true);
+    }
+  });
+ });
