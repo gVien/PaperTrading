7d69c6fedf86d4696d79b8fafb8a18c3afeab232
gVien <wavetech86@gmail.com>
2015-11-28 12:50:10 -0800

    add TDD for relationship model

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationship = Relationship.new(followed_id: 1, follower_id: 2)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should have followed_id" do
+    @relationshp.followed_id = ""
+    assert_not @relationshp.valid?  #pass if valid? returns false
+  end
+
+  test "should have follower_id" do
+    @relationshp.follower_id = ""
+    assert_not @relationshp.valid?  #pass if valid? returns false
+  end
 end
