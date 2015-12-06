2de3d013b9d7e59b44bc4f65ff02dd4a397efb39
gVien <wavetech86@gmail.com>
2015-12-06 14:10:38 -0800

    update create/destroy methods in relationship ctrler,  pass test

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,18 @@
 class RelationshipsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
   def create
+    # followed_id is the id of the user being followed, as shown in hidden field (users/_followed.html.erb)
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user_path(user) # or simply `redirected_to user`
   end
 
   def destroy
+    # find the relationship between the current_user and the one being followed (shown in params[:id]), and obtain user info
+    # i.e. find the user the current_user is following
+    user = Relationship.find(params[:id]).followed  #followed is the user that the current_user (follower) is following
+    current_user.unfollow(user)
+    redirect_to user_path(user)
   end
 end
