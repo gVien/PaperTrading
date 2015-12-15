627e2cff10aa379434dd59c76d4d6a8892a40070
gVien <wavetech86@gmail.com>
2015-12-14 23:28:12 -0800

    only include top 10 of each top market trend

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -6,12 +6,12 @@ module StaticPagesHelper
 
   def market_trend
     {
-      "Top Active": TradeKing.get_topactive,
-      "Top Volume": TradeKing.get_topvolume,
-      "Top $ Losers": TradeKing.get_toplosers,
-      "Top $ Gainers": TradeKing.get_topgainers,
-      "Top % Losers": TradeKing.get_top_pct_losers,
-      "Top % Gainers": TradeKing.get_top_pct_gainers
+      "Top Active": TradeKing.get_topactive.first(10),
+      "Top Volume": TradeKing.get_topvolume.first(10),
+      "Top $ Losers": TradeKing.get_toplosers.first(10),
+      "Top $ Gainers": TradeKing.get_topgainers.first(10),
+      "Top % Losers": TradeKing.get_top_pct_losers.first(10),
+      "Top % Gainers": TradeKing.get_top_pct_gainers.first(10)
     }
   end
 
