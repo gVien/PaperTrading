4f70871e042c11aa6125dcb6c8d9a3f74cb0b1a3
gVien <wavetech86@gmail.com>
2015-12-26 23:23:58 -0800

    add search route for global search form

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,9 @@ Rails.application.routes.draw do
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :posts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  get "search" => "searches#show"
+
+  # resources :search, only: [:index, :create, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
