e024936a0d36dc9c4db07fae88c477bf6a3e8037
gVien <wavetech86@gmail.com>
2015-12-27 23:24:48 -0800

    implement search of stocks + preliminary views for stock results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,16 @@
 class SearchesController < ApplicationController
+
+  def show
+    query = params[:symbol].upcase
+    @stock = Stock.search(query)
+    if @stock.respond_to?(:each) #duck typing
+      render "show_stocks_list"
+    else
+      if @stock
+        render @searches
+      else
+        render "show_stock_not_found"
+      end
+    end
+  end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,8 +3,23 @@ class Stock < ActiveRecord::Base
   has_many :watchlists, through: :watchlists_stocks
   before_save :upcase_symbol
 
+  default_scope -> { order(symbol: :ASC) } # redefine the default order of symbol
+
   validates :symbol, presence: true
   validates :company_name, presence: true
+
+  def self.search(query)
+    # if symbol exists, display the matched stock
+    # elsif blank && lists of stock exists, display lists of stocks partially matching query unless it's empty
+    # else return nil
+    if stock = find_by(symbol: query)
+      stock
+    elsif !query.blank? && list_of_stocks = where('symbol LIKE ?', "%#{query}%") # query must not be blank!
+      list_of_stocks unless list_of_stocks.blank?  #empty array should be filtered
+    else # covers when query is nil + empty
+      nil
+    end
+  end
 
   private
     def upcase_symbol
