ed7e8bd8a353e6a70573e6034ac6fe6572cb2d53
gVien <wavetech86@gmail.com>
2016-01-01 22:57:02 -0800

    use symbol in stock url instead of id & upcase search query

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,10 @@
 class StocksController < ApplicationController
   def show
-    @stock = Stock.find(params[:id])  # id from redirect
+    @stock = Stock.find_by_symbol(params[:id])  # id from redirect
+    if @stock
+      @stock
+    else
+      redirect_to search_path(symbol: params[:id].upcase)
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -14,11 +14,17 @@ class Stock < ActiveRecord::Base
     # else return nil
     if stock = find_by(symbol: query)
       stock
-    elsif !query.blank? && list_of_stocks = where('symbol LIKE ?', "%#{query}%") # query must not be blank!
+    elsif !query.blank? && list_of_stocks = where('symbol LIKE ?', "%#{query.upcase}%") # query must not be blank!
       list_of_stocks unless list_of_stocks.blank?  #empty array should be filtered
     else # covers when query is nil + empty
       nil
     end
+  end
+
+  # redefining to_param to use symbol instead of id (e.g. /symbol/ABC instead of /symbol/1)
+  # friendly_id can do a better job (added to Gemfile) but this works well for current purpose
+  def to_param
+    "#{symbol}"
   end
 
   private
